8f5ca2299cd351aecdac5782cc54bbd1be2adaea
Pranto Deb <rajprit0m@outlook.com>
2024-07-16 02:43:28 +0600

    [Navbar] : Navbar design Completed

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/Root";
 import Error from "../pages/Error/Error";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Authentication/Login";
+import Register from "../pages/Authentication/Register";
 
 export const router = createBrowserRouter([
     {
@@ -10,6 +12,16 @@ export const router = createBrowserRouter([
         errorElement: <Error />,
         children: [
             { path: "/", element: <Home /> },
+            { path: "/who-we-are", element: 'who-we-are' },
+            { path: "/branch", element: 'branch' },
+            { path: "/send-money", element: 'send money' },
+            { path: "/cash-out", element: 'cash-out' },
+            { path: "/pay-bill", element: 'pay-bill' },
+            { path: "/mobile-recharge", element: 'mobile-recharge' },
+            { path: "/paynect-to-bank", element: 'paynect-to-bank' },
+            { path: "/loan", element: 'loan' },
+            { path: "/login", element: <Login /> },
+            { path: "/register", element: <Register /> },
         ],
     },
 ]);
